2c44d588c42d957f6d9a4b7e6d70a69022d3eea6
hraym <harryraym@gmail.com>
2024-08-16 22:09:30 +0900

    Updated Default Settings
    Now collects all indicators by default, unless specified by the user

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import asyncio
 import argparse
 from src.pipeline import get_world_bank_data
+from src.dashboard import create_dashboard, run_dashboard
+from src.indicators_config import indicators, get_all_indicator_codes, get_theme_for_indicator
 
-async def main(indicator_codes: list, countries: list, start_year: int, end_year: int):
-    data = await get_world_bank_data(indicator_codes, countries, start_year, end_year)
+async def main(indicator_codes: list, countries: list, start_year: int, end_year: int, run_viz: bool):
+    if not indicator_codes:
+        indicator_codes = get_all_indicator_codes()
     
-    for indicator, df in data.items():
-        print(f"\nData for {indicator}:")
-        print(df.head())
+    data, indicator_mapping = await get_world_bank_data(indicator_codes, countries, start_year, end_year)
     
-    # Add any additional processing or saving logic here
+    if not run_viz:
+        
+        for indicator, df in data.items():
+            print(f"\nData for {indicator} ({indicator_mapping.get(indicator, indicator)}):")
+            
+    if run_viz:
+        run_dashboard(data, indicators, indicator_mapping)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fetch World Bank data for specified indicators and countries.")
-    parser.add_argument("--indicators", nargs="+", required=True, help="List of indicator codes")
+    parser = argparse.ArgumentParser(description="Fetch World Bank data and visualize it.")
+    parser.add_argument("--indicators", nargs="+", choices=get_all_indicator_codes(), help="List of indicator codes (optional)")
     parser.add_argument("--countries", nargs="+", required=True, help="List of country codes")
-    parser.add_argument("--start_year", type=int, default=1960, help="Start year for data retrieval")
-    parser.add_argument("--end_year", type=int, help="End year for data retrieval")
-
+    parser.add_argument("--start_year", type=int, default=2000, help="Start year for data retrieval")
+    parser.add_argument("--end_year", type=int, default=2020, help="End year for data retrieval")
+    parser.add_argument("--visualize", action="store_true", help="Run the visualization dashboard")
+    
     args = parser.parse_args()
-
-    asyncio.run(main(args.indicators, args.countries, args.start_year, args.end_year))
+    
+    asyncio.run(main(args.indicators, args.countries, args.start_year, args.end_year, args.visualize))
